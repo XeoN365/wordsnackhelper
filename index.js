6e06a6fed640e6b86643cd41c9b830532ab6989a
xeon365 <xee3oon@gmail.com>
2017-11-01 17:28:21 +0000

    Initial commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on('message', (payload, chat) => {
        
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-          string = JSON.parse(data).all;
+          string = JSON.parse(data);
         
         });
        
